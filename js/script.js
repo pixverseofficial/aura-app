992e8c2ddd12655fffba4bdcdb7b23dec3fedf88
vex.designs <pixverse.official00@gmail.com>
2026-07-02 06:03:35 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,3 +1,43 @@
 import "./firebase.js";
 
 console.log("AURA Loaded");
+
+import { auth } from "./firebase.js";
+import {
+  createUserWithEmailAndPassword
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-auth.js";
+
+const signupForm = document.getElementById("signupForm");
+
+if (signupForm) {
+
+  signupForm.addEventListener("submit", async (e) => {
+
+    e.preventDefault();
+
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("password").value;
+    const confirmPassword = document.getElementById("confirmPassword").value;
+
+    if (password !== confirmPassword) {
+      alert("Passwords do not match.");
+      return;
+    }
+
+    try {
+
+      await createUserWithEmailAndPassword(auth, email, password);
+
+      alert("Account created successfully!");
+
+      window.location.href = "home.html";
+
+    } catch (error) {
+
+      alert(error.message);
+
+    }
+
+  });
+
+}
